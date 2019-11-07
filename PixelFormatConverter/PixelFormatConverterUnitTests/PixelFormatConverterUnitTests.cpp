13482f8607f076e364038c1da120152d856cf218
s.zaplatnikov <s.zaplatnikov@riftek.com>
2019-11-07 21:48:05 +0300

    Unit test project was created. Added OpenCV.

---
+++ PixelFormatConverter/PixelFormatConverterUnitTests/PixelFormatConverterUnitTests.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace PixelFormatConverterUnitTests
+{
+	TEST_CLASS(PixelFormatConverterUnitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+
+
+
+
+		}
+	};
+}
